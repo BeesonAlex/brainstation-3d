117a7f5abc23c9fe41bbc6d09680afb51b47f062
Alex Beeson <beeson.alexander@gmail.com>
2019-05-24 15:37:45 -0400

    added datasets for 3 graphs

--- client/src/data/popularityData.js
+++ client/src/data/popularityData.js
@@ -1,36 +1,22 @@
 export default {
     labels: [
-        'January',
-        'February',
-        'March',
-        'April',
-        'May',
-        'June',
-        'July',
-        'August',
-        'September',
-        'October',
-        'November',
-        'December'
+        'Pop Culture',
+        'Technology',
+        'Science',
+        'Entertainment',
     ],
     datasets: [
         {
-            label: '2018 Job postings (thousands)',
+            label: 'Popularity',
             backgroundColor: '#9CC69B',
             borderColor: '#79B4A9',
             data: [
-                { x: '2018-01-01', y: 35 },
-                { x: '2018-02-01', y: 75 },
-                { x: '2018-03-01', y: 25 },
-                { x: '2018-04-01', y: 95 },
-                { x: '2018-05-01', y: 45 },
-                { x: '2018-06-01', y: 55 },
-                { x: '2018-07-01', y: 35 },
-                { x: '2018-08-01', y: 95 },
-                { x: '2018-09-01', y: 45 },
-                { x: '2018-10-01', y: 25 },
-                { x: '2018-11-01', y: 55 },
-                { x: '2018-12-01', y: 75 },
+                { x: '2018-01-01', y: 9445 },
+                { x: '2018-02-01', y: 8581 },
+                { x: '2018-03-01', y: 8427 },
+                { x: '2018-04-01', y: 7057 },
+                { x: '2018-04-01', y: 0 },
+                { x: '2018-04-01', y: 10000 },
             ]
         },
         
